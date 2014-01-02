ae498634992a0a77ab0fe7c3281b81b8298a236c
oldlam <oldlam1117@hotmail.com>
2014-01-02 16:25:34 +0800

    [bugfix] syntax bugfix on /uses/session

--- vendor/slim/slim/index.php
+++ vendor/slim/slim/index.php
@@ -218,22 +218,22 @@ $app->post('/users/:email', function($email) use ($app){
 $app->post('/users/session/:email', function($email) use ($app){
 	$action = $app->request()->params('action');
 	$password= $app->request()->params('password');
-	$returnValue = array();
-	$returnValue['result'] = false;
+	$result = array();
+	$result['result'] = false;
 	if($action == "login"){
 		$returnValue = DB::queryFirstRow("SELECT * FROM user WHERE email = %s AND password = %s" , $email, $password);
-		if(sizeof($returnValue) ==1){
-		$result['result'] = true;
-		$result['user']['first_name'] = $returnValue['first_name'];
-		$result['user']['last_name'] = $returnValue['last_name'];
-		$result['user']['email'] = $returnValue['email'];
-		$result['user']['phone'] = $returnValue['phone'];
-		$result['user']['user_id'] = $returnValue['user_id'];
-		$result['user']['token'] = '1231231234';
+		if(!is_null($returnValue)){
+			$result['result'] = true;
+			$result['user']['first_name'] = $returnValue['first_name'];
+			$result['user']['last_name'] = $returnValue['last_name'];
+			$result['user']['email'] = $returnValue['email'];
+			$result['user']['phone'] = $returnValue['phone'];
+			$result['user']['user_id'] = $returnValue['user_id'];
+			$result['user']['token'] = '1231231234';
+		}
 	}
 	//var_dump($rs);
-	echo json_encode($returnValue);
-
+	echo json_encode($result);
 });
 
 
