f49ce031c4d30ba8a64dfaaab30c90adb90bd97f
Ryder <ryderlee@live.com>
2014-03-19 12:04:52 +0800

    Update : RestaurantTableBookingModule
    Many fix

--- vendor/slim/slim/RestaurantTableBookingModule.php
+++ vendor/slim/slim/RestaurantTableBookingModule.php
@@ -114,11 +114,16 @@ class RestaurantTableBookingModule{
 	private static $theDate = null;
 
 
+	private static $bookingStartDatetime = null;
+	private static $bookingEndDatetime = null;
+	private static $bookingStartTimestamp= null;
+	private static $bookingEndTimestamp= null;
+	private static $sessionDate = null;
 	private static $sessionStartDatetime = null;
 	private static $sessionEndDatetime = null;
-	private static $sessionDatetime = null;
+	private static $sessionStartTimestamp= null;
+	private static $sessionEndTimestamp= null;
 
-	private $isLockedDB = false;
 
 	private static function setStaticVar($mid, $bookingDatetime, $covers){
 		if(isset(RestaurantTableBookingModule::$mid) && ( RestaurantTableBookingModule::$mid == $mid) && 
@@ -131,14 +136,25 @@ class RestaurantTableBookingModule{
 			$merchantTemplate = $restaurantTemplateService->getTemplate($mid, $bookingDatetime);
 			
 			/*$session = $merchantTemplate->getSession($bookingDatetime);
-			RestaurantTableBookingModule::$sessionStartDatetime = $session->getStartTime();
-			RestaurantTableBookingModule::$sessionEndDatetime = $session->getStartTime() + 60 * $session->getSessionLength();
+			RestaurantTableBookingModule::$bookingStartDatetime = $session->getStartTime();
+			RestaurantTableBookingModule::$bookingEndDatetime = $session->getStartTime() + 60 * $session->getSessionLength();
 			*/
 			//for testing purpose
 
-			RestaurantTableBookingModule::$sessionStartDatetime = $bookingDatetime;			
-			RestaurantTableBookingModule::$sessionEndDatetime =  RestaurantTableBookingModule::$sessionStartDatetime + 60 * 120;
+			RestaurantTableBookingModule::$bookingStartDatetime = $bookingDatetime;			
+			$ts = strtotime($bookingDatetime);
+			RestaurantTableBookingModule::$sessionDate = date("Y-n-j H:i:s", mktime(0, 0, 0, date("n", $ts), date("j", $ts), date("Y", $ts))) ;	
+			RestaurantTableBookingModule::$sessionStartDatetime = date("Y-n-j H:i:s", mktime(18, 0, 0, date("n", $ts), date("j", $ts), date("Y", $ts))) ;	
+			RestaurantTableBookingModule::$sessionEndDatetime = date("Y-n-j H:i:s", mktime(25, 0, 0, date("n", $ts), date("j", $ts), date("Y", $ts))) ;	
+			RestaurantTableBookingModule::$bookingEndDatetime =  date("Y-n-j H:i:s", strtotime(RestaurantTableBookingModule::$bookingStartDatetime) + 60 * 120);
 
+
+			
+			RestaurantTableBookingModule::$sessionStartTimestamp = strtotime(RestaurantTableBookingModule::$sessionStartDatetime);
+			RestaurantTableBookingModule::$sessionEndTimestamp = strtotime(RestaurantTableBookingModule::$sessionEndDatetime);
+			RestaurantTableBookingModule::$bookingStartTimestamp = strtotime(RestaurantTableBookingModule::$bookingStartDatetime);
+			RestaurantTableBookingModule::$bookingEndTimestamp = strtotime(RestaurantTableBookingModule::$bookingEndDatetime);
+			
 
 			global $redis;
 			RestaurantTableBookingModule::$redis = $redis;
@@ -146,31 +162,32 @@ class RestaurantTableBookingModule{
 		}
 	}
 	private static function getKey($cover){
-		return ("restaurantTableCache|" . RestaurantTableBookingModule::$merchantID . '|' . date('Ymd', RestaurantTableBookingModule::$sessionDatetime) . '|' . $cover);
+		return ("restaurantTableCache|" . RestaurantTableBookingModule::$merchantID . '|' . date('Ymd', strtotime(RestaurantTableBookingModule::$sessionDate)) . '|' . $cover);
 	}
 	public static function isAvailable($mid, $bookingDatetime, $covers){
 		RestaurantTableBookingModule::setStaticVar($mid, $bookingDatetime, $covers);
 		$setting = getMerchantSettings($mid);
-
 		$bookingLength = intval($setting['tableBookingLength']);
 		$bookingInterval = intval($setting['tableBookingInterval']);
-		$cache = RestaurantTableBookingModule::getCache($cover);
 		$returnValue = true;
-		$startDatetime = RestaurantTableBookingModule::$sessionStartDatetime;
-		for($i = 0; ($i < $bookingLength) && ($returnValue == 1); $i = $i + $bookingInterval){
-			$tempDate = mktime(date("H", $stateDatetime), date("i", $startDatetime) + $i, 0, date("m", $startDatetime), date("d", $startDatetime), date("Y", $startDatetime));
+		
+		$cache = RestaurantTableBookingModule::getCache($mid, $bookingDatetime, $covers);
+		$startDatetime = RestaurantTableBookingModule::$bookingStartDatetime;
+		$startTimestamp = RestaurantTableBookingModule::$bookingStartTimestamp;
+		for($i = 0; ($i < $bookingLength) && ($returnValue == true); $i = $i + $bookingInterval){
+			$tempDate = mktime(date("H", $startTimestamp), date("i", $startTimestamp) + $i, 0, date("m", $startTimestamp), date("d", strtotime($startTimestamp)), date("Y", $startTimestamp));
 			//echo date("Ymd His\n", $tempDate);
 			//echo date("Ymd His", $this->theDate);
 			$thekey = date('Hi', $tempDate);
+			//var_dump($cache);
 			$returnValue = (intval($cache[$thekey]) > 0);
 			if($returnValue == false)
 				break;
 		}
-		echo "returnValue : " . $returnValue?"TRUE":"FALSE";
 		return $returnValue;
 	}	
 	public static function lockDB($tableID){
-		$key = $this->getKey($tableID);
+		$key = RestaurantTableBookingModule::getKey($tableID);
 		DB::startTransaction();
 		//$sql = "UPDATE cache_restaurant_tables SET locked=1 WHERE locked=0 AND thekey='" . $key . "'";
 		DB::update('cache_restaurant_tables', array('locked'=>1), "locked=%d AND thekey=%s", 0, $key);
@@ -182,45 +199,43 @@ class RestaurantTableBookingModule{
 			DB::commit();
 			return true;
 		}
+
 	}
 	public static function isLockReady($tableID){
 		$key = RestaurantTableBookingModule::getKey($tableID);
 
 		$sql = "select count(*) from cache_restaurant_tables where thekey='" . $key . "'";
-		if(!RestaurantTableBookingModule::$isLockedDB){
-			if(DB::queryFirstField($sql) == 0){
-				DB::insert('cache_restaurant_tables', array('thekey'=>$key, 'locked'=>0));
-			}
+		if(DB::queryFirstField($sql) == 0){
+			DB::insert('cache_restaurant_tables', array('thekey'=>$key, 'locked'=>0));
 		}
 		return true;
 	}
 	public static function lock($mid, $bookingDatetime, $covers, $table, $bookingLength){
-		$this->setStaticVar($mid, $bookingDatetime, $covers);
+		RestaurantTableBookingModule::setStaticVar($mid, $bookingDatetime, $covers);
 		$mSetting = getMerchantSettings($mid);
 
 		$bookingLength = intval($mSetting['tableBookingLength']);
 		$bookingInterval = intval($mSetting['tableBookingInterval']);
-		$startDatetime = RestaurantTableBookingModule::$sessionStartDatetime;
-		$cache = RestaurantTableBookingModule::getCache($cover);
+		$startDatetime = RestaurantTableBookingModule::$bookingStartDatetime;
 
-		$key = RestaurantTableBookingModule::getKey($cover);
+		$key = RestaurantTableBookingModule::getKey($covers);
 		
-		if(RestaurantTableBookingModule::isLockReady($table->getTableId()) || !RestaurantTableBookingModule::lockDB($table->getTableId())){
-			return -1;
+		if(RestaurantTableBookingModule::isLockReady($table->getTableId()) && RestaurantTableBookingModule::lockDB($table->getTableId())){
+			return true;
 		}else{
+			return false ;
 		}
-		return true;
 		
 	}
 	
 	public static function unlock($mid, $bookingDatetime, $covers, $restaurantTable, $bookingLength){
-		$this->setStaticVar($mid, $bookingDatetime, $covers);
+		RestaurantTableBookingModule::setStaticVar($mid, $bookingDatetime, $covers);
 		$mSetting = getMerchantSettings($mid);
 		$key = RestaurantTableBookingModule::getKey($restaurantTable->getTableId());
 		$sql = "SELECT count(*) FROM cache_restaurant_tables WHERE thekey='" . $key . "'";
 		
 		if(DB::queryFirstField($sql) == 0){
-			RestaurantTableBookingModule::resetCache();
+			RestaurantTableBookingModule::resetCache($mid, $bookingDatetime, $covers);
 		}
 		if(RestaurantTableBookingModule::unlockDB($restaurantTable->getTableId())){
 			return 1;
@@ -233,10 +248,10 @@ class RestaurantTableBookingModule{
 	public static function unlockDB($tableID){
 		$key = RestaurantTableBookingModule::getKey($tableID);
 		DB::startTransaction();
-		DB::update('cache_restaurant_tables', array('locked'=>0), "locked=%d AND thekey=%s", 1, $key);
+		DB::update('cache_restaurant_tables', array('locked'=>0), "thekey=%s", $key);
 		if(DB::affectedRows() == 0){
-			//ERROR!!!
-			DB::rollback();
+			//ERROR!!! but unlock it whatever
+			DB::commit();
 			return false;
 		}else{
 			DB::commit();
@@ -248,25 +263,33 @@ class RestaurantTableBookingModule{
 
 		RestaurantTableBookingModule::setStaticVar($mid, $bookingDatetime, $covers);
 		$mSetting = getMerchantSettings($mid);
+		if(RestaurantTableBookingModule::isAvailable($mid, $bookingDatetime, $covers, $restaurantTable, $bookingLength)){
+			RestaurantTableBookingModule::resetCache($mid, $bookingDatetime, $covers);
+			return true;
+		}else{
+			return false;
+		}
+		/*
 		$bookingLength = intval($mSetting['tableBookingLength']);
 		$bookingInterval = intval($mSetting['tableBookingInterval']);
-		$bookingStart = RestaurantTableBookingModule::$sessionStartDatetime;
-		$bookingEnd = RestaurantTableBookingModule::$sessionEndDatetime;
+		$bookingStart = RestaurantTableBookingModule::$bookingStartDatetime;
+		$bookingEnd = RestaurantTableBookingModule::$bookingEndDatetime;
+		$bookingStartTimestamp = RestaurantTableBookingModule::$bookingStartTimestamp;
+		$bookingEndTimestamp = RestaurantTableBookingModule::$bookingEndTimestamp;
 		$passed = true;
-		$tempResult = array();
+		//$tempResult = array();
 		//$this->redis->multi();
-		$tempResult[] = $key;
+		//$tempResult[] = $key;
 
-		$sql = "SELECT * FROM restaurant_table WHERE table_id = $d" ;
 		$tableID = $restaurantTable->getTableId();
-		$rs = DB::queryFirstRow($sql, $tableID);
-		$minCover = $rs['min_cover'];
-		$maxCover = $rs['max_cover'];
+		//$sql = "SELECT * FROM restaurant_table WHERE restaurant_table_id = %d" ;
+		//$rs = DB::queryFirstRow($sql, $tableID);
+		$minCover = $restaurantTable->getMinCover();
+		$maxCover = $restaurantTable->getMaxCover();
 		for($cover = intval($minCover); $cover <= intval($maxCover) ; $cover++){
-			$key = RestaurantTableBookingModule::getKey($cover);
-			$tempArr = RestaurantTableBookingModule::$redis->hgetall($key);
-			for($i = 0; ($i < $bookingLength) && $passed; $i = $i + $bookingInterval){
-				$tempDate = mktime(date("H", $bookingStart), date("i", $bookingStart) + $i, 0, date("m", $bookingStart), date("d", $bookingStart), date("Y", $bookingStart));
+			$tempArr = RestaurantTableBookingModule::getCache($mid, $bookingDatetime, $covers);
+			for($i = 0; $passed && ($i < $bookingLength) ; $i = $i + $bookingInterval){
+				$tempDate = mktime(date("H", $bookingStartTimestamp), date("i", $bookingStartTimestamp) + $i, 0, date("m", $bookingStartTimestamp), date("d", $bookingStartTimestamp), date("Y", $bookingStartTimestamp));
 				
 				$thekey = date('Hi', $tempDate);
 				$passed = (intval($tempArr[$thekey]) > 0);
@@ -274,7 +297,7 @@ class RestaurantTableBookingModule{
 				//$tempResult[$thekey] = intval($tempArr[$thekey]) - 1;
 			}	
 			if($passed){
-				RestaurantTableBookingModule::resetCache();
+				RestaurantTableBookingModule::resetCache($mid, $bookingDatetime, $covers);
 				//call_user_func_array(array($this->redis, "hmset"), $tempResult);
 			}else{
 				//$this->redis->discard();
@@ -282,6 +305,7 @@ class RestaurantTableBookingModule{
 		}
 
 		return $passed;
+		*/
 	}
 
 	public static function resetCache($mid, $bookingDatetime, $covers){
@@ -290,14 +314,14 @@ class RestaurantTableBookingModule{
 
 		$seatArr = array();
 		
+		//echo "resetCache\n";
+		/*
 		$bookingSeatList = ($OSCache['tableSeatOptions']);
 		$tmpArr = explode(",", $bookingSeatList); 
-		/*
 		foreach($tmpArr as $v){
 			$seatArr[] = intval($v);
 		}
-		foreach($cache as $key=>$value){
-			
+		foreach($cache as $key=>$value){ 
 			if((substr($key, 0, 8) == 'tableFor') && (intval(substr($key, 8)) > 0)){
 				$seatArr[] = intval(substr($key, 8));
 			}
@@ -305,12 +329,17 @@ class RestaurantTableBookingModule{
 		*/
 		$bookingLength = intval($OSCache['tableBookingLength']);
 		$bookingInterval = intval($OSCache['tableBookingInterval']);
-		$bookingStartDatetime = RestaurantTableBookingModule::$sessionStartDatetime;
-		$bookingEndDatetime = RestaurantTableBookingModule::$sessionEndDatetime;
+		$bookingStartDatetime = RestaurantTableBookingModule::$bookingStartDatetime;
+		$bookingEndDatetime = RestaurantTableBookingModule::$bookingEndDatetime;
+		$bookingStartTimestamp= RestaurantTableBookingModule::$bookingStartTimestamp;
+		$bookingEndTimestamp= RestaurantTableBookingModule::$bookingEndTimestamp;
+		$sessionStartTimestamp= RestaurantTableBookingModule::$sessionStartTimestamp;
+		$sessionEndTimestamp= RestaurantTableBookingModule::$sessionEndTimestamp;
 		//$bookingStartDatetime = mktime(intval(substr($bookingStart, 0, 2)), intval(substr($bookingStart, 2, 2)),   0,  date("n", $this->theDate), date("j", $this->theDate), date("Y", $this->theDate));
 		//$bookingEndDatetime = mktime(intval(substr($bookingStart, 0, 2)), intval(substr($bookingStart, 2, 2)) + intval($OSCache['tableBookingEnd']),   0,  date("n", $bookingStartDatetime), date("j", $bookingStartDatetime), date("Y", $bookingStartDatetime));
 		
 		$bookingCoverList = explode(',', $OSCache['tableCoverList']);
+
 		$maxBookingCover = max($bookingCoverList);
 		$minBookingCover = min($bookingCoverList);
 		$sql = "SELECT * FROM restaurant_table WHERE merchant_id = %d AND max_cover <= %d AND min_cover >= %d";
@@ -319,9 +348,9 @@ class RestaurantTableBookingModule{
 		foreach($bookingCoverList as $cover){
 			for($i = 0; $i < sizeof($rs) ; $i++){
 				if(intval($rs[$i]['min_cover']) <= $cover && intval($rs[$i]['max_cover']) >= $cover){
-					for($j = $bookingStartDatetime; $j <= $bookingEndDatetime; $j = $j + ($bookingInterval * 60)){
+					for($j = $sessionStartTimestamp; $j <= $sessionEndTimestamp; $j = $j + ($bookingInterval * 60) ){
 						$timeKey = date('Hi', $j);
-						if(!isset($tableArr[$cover]["" . $timeKey]))			
+						if(!isset($tableArr[$cover]["" . $timeKey]))
 							$tableArr[$cover]["" . $timeKey] = 0;
 						$tableArr[$cover]["" . $timeKey] ++;
 					}
@@ -329,16 +358,17 @@ class RestaurantTableBookingModule{
 			}
 		}
 		$sql = "SELECT *, (DATE_ADD(b.booking_ts, INTERVAL b.booking_length MINUTE)) AS booking_end_ts FROM booking_restaurant_table brt LEFT JOIN restaurant_table rt ON brt.restaurant_table_id = rt.restaurant_table_id LEFT JOIN booking b ON brt.booking_id = b.booking_id WHERE rt.merchant_id = %d AND (b.booking_ts BETWEEN %s AND %s)";
-		$rs2 = DB::query($sql , $mid, date("Y-m-d H:i:s", $bookingStartDatetime), date("Y-m-d H:i:s", $bookingEndDatetime));
+		$rs2 = DB::query($sql , $mid, date("Y-m-d H:i:s", $sessionStartTimestamp), date("Y-m-d H:i:s", $sessionEndTimestamp));
 		
-		echo sizeof($rs2);
 		for($j = 0; $j < sizeof($rs2); $j++){
-			for($k = intval($rs[$j]['min_cover']) ; $k <= intval($rs[$j]['max_cover']) ; $k++){
+			for($k = intval($rs2[$j]['min_cover']) ; $k <= intval($rs2[$j]['max_cover']) ; $k++){
 				if(in_array($k, $bookingCoverList)){
 					for($t = convertDatetime($rs2[$j]['booking_ts']) ; $t < convertDatetime($rs2[$j]['booking_end_ts']) ; $t = $t + 60){
 						$timeKey = date('Hi', $t);
-						if(isset($tableArr[$k]["" . $timeKey]))
+						if(isset($tableArr[$k]["" . $timeKey])){
 							$tableArr[$k]["" . $timeKey] -- ;
+
+						}
 					}
 				}
 			}
@@ -348,14 +378,14 @@ class RestaurantTableBookingModule{
 			$key = RestaurantTableBookingModule::getKey($cover);
 			RestaurantTableBookingModule::$redis->del($key);
 			RestaurantTableBookingModule::$redis->multi();
-			for($i = $bookingStartDatetime ; $i <= $bookingEndDatetime ; $i = $i + ($bookingInterval * 60)){
+			for($i = $sessionStartTimestamp; $i <= $sessionEndTimestamp; $i = $i + ($bookingInterval * 60)){
 				if(isset($tableArr[$cover])){
 					$timeKey = date('Hi', $i);
-					echo $tableArr[$cover]["" . $timeKey];
+					//echo $tableArr[$cover]["" . $timeKey];
 					RestaurantTableBookingModule::$redis->hmset($key, $timeKey, $tableArr[$cover]["" . $timeKey]);		
 				}
 			}
-			$RestaurantTableBookingModule::$redis->exec();
+			RestaurantTableBookingModule::$redis->exec();
 		}
 		
 
@@ -365,20 +395,23 @@ class RestaurantTableBookingModule{
 		RestaurantTableBookingModule::setStaticVar($mid, $bookingDatetime, $covers);
 		$OSCache = getMerchantSettings($mid);
 		$returnValue = array();
-
-		$key = RestaurantTableBookingModule::getKey($seats);
-
+		$key = RestaurantTableBookingModule::getKey($covers);
 		$tempArr = array();
-
 		$tempArr = RestaurantTableBookingModule::$redis->hgetall($key);
 		$generated = false;
-
-		while(!$generated && sizeof($tempArr) == 0){
+		$safeCount = 3;
+		while($safeCount > 0 && (!$generated) && sizeof($tempArr) == 0 ){
 			$generated = true;
-			RestaurantTableBookingModule::resetCache();
+			$safeCount -- ;
+			RestaurantTableBookingModule::resetCache($mid, $bookingDatetime, $covers);
 		}
 		if(sizeof($tempArr) == 0){
-			return 0;
+			if($generated){
+				$tempArr = RestaurantTableBookingModule::$redis->hgetall($key);
+				return $tempArr;
+			}else{
+				return 0;
+			}
 		}else{
 			return $tempArr;
 		}
