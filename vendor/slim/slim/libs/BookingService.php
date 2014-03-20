909a2d580f18e59b98b1fa2688def2b41c913635
oldlam <oldlam1117@hotmail.com>
2014-03-20 13:13:36 +0800

    [UPDATE] update interface

--- vendor/slim/slim/libs/BookingService.php
+++ vendor/slim/slim/libs/BookingService.php
@@ -7,7 +7,7 @@ $restaurantTemplateService = new RestaurantTemplateService();
 interface BookingServiceInterface {
 	public function getBestTable($merchantId, $datetime, $noOfParticipants);
 	public function makeBooking($userId, $merchantId, $isGuest, $sessionId, $firstName, $lastName, $phone, $datetime, $noOfParticipants, $specialRequest);
-	public function makeBookingByMerchant($tableId, $merchantId, $firstName, $lastName, $phone, $datetime, $noOfParticipants, $specialRequest);
+	public function makeBookingByMerchant($userId, $merchantId, $firstName, $lastName, $phone, $datetime, $noOfParticipants, $specialRequest, $status, $attendance, $arrOfTables, $bookingLength);
 }
 
 class RestaurantBookingService implements BookingServiceInterface {
